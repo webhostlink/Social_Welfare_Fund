bff1c796c69c9c16811d6dbc466c6811861c3cef
webhostlink <152862607+webhostlink@users.noreply.github.com>
2023-12-04 16:32:30 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,3 +3,27 @@
 function learnMore() {
     alert('Learn more functionality can be added here!');
 }
+
+function exploreTeam() {
+    alert('Explore Team functionality can be added here!');
+}
+
+function programs() {
+    alert('Programs functionality can be added here!');
+}
+
+function explorePrograms() {
+    alert('Explore Programs functionality can be added here!');
+}
+
+function get-involved() {
+    alert('Get-involved functionality can be added here!');
+}
+
+function gallery() {
+    alert('Gallery functionality can be added here!');
+}
+
+function contact() {
+    alert('Contact functionality can be added here!');
+}
